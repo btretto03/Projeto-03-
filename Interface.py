763189bfbb5435576949d3b50270963268f36424
Bruno Tretto <tretto98@gmail.com>
2025-06-29 17:11:48 -0300

    erro do ç
    Tava dando erro no ç mudei pra opcoes

--- Interface.py
+++ Interface.py
@@ -30,7 +30,7 @@ FRACO =  "\033[0;30m"
 a = True
     
 while a:
-    opções_do_menu = ["Criar nova lista de tarefas", "Criar nova tarefa", "Modificar tarefas", "Visualizar tarefas", "Sair"]
+    opcoes_do_menu = ["Criar nova lista de tarefas", "Criar nova tarefa", "Modificar tarefas", "Visualizar tarefas", "Sair"]
     opcao = 0
 
     if EH_WINDOWS: #Caso o sistema operacional seja windows, usamos esse codigo para capturar a tecla
@@ -101,7 +101,7 @@ while a:
             + WNEGRITO
             + RESET 
     )
-        for i, item in enumerate(opções_do_menu):
+        for i, item in enumerate(opcoes_do_menu):
             if i == opcao and opcao != 3:
                 print( " "* 2 + SUBLINHADO + " "* 3 + f"  {item}     " + RESET)
             elif i == opcao and opcao == 3:
@@ -117,30 +117,30 @@ while a:
         mostrar_menu()
         tecla = tecla_apertada()
         if tecla == 'cima':
-            opcao = (opcao - 1) % len(opções_do_menu)
+            opcao = (opcao - 1) % len(opcoes_do_menu)
             if opcao == -1:
                 opcao = 4
                 continue
         elif tecla == 'baixo':
-            opcao = (opcao + 1) % len(opções_do_menu)
+            opcao = (opcao + 1) % len(opcoes_do_menu)
             if opcao == 5:
                 opcao = 0
                 continue
         elif tecla == 'enter':
             limpar()
-            if "Criar nova lista de tarefas" in opções_do_menu[opcao]:
+            if "Criar nova lista de tarefas" in opcoes_do_menu[opcao]:
                 while True:
                     exec(open("criar_lista_de_tarefas.py", encoding='utf-8').read())
                     
-            elif "Criar nova tarefa" in opções_do_menu[opcao]:
+            elif "Criar nova tarefa" in opcoes_do_menu[opcao]:
                 exec(open("criar_tarefas.py", encoding='utf-8').read())
-            elif "Modificar tarefas" in opções_do_menu[opcao]:
+            elif "Modificar tarefas" in opcoes_do_menu[opcao]:
                 exec(open("Modificar_tarefas.py",encoding='utf-8').read())
-            elif "Visualizar tarefas" in opções_do_menu[opcao]:
+            elif "Visualizar tarefas" in opcoes_do_menu[opcao]:
                 exec(open("Visualisar_tarefas.py",encoding='utf-8').read())
             
                     
-            elif "Sair" in opções_do_menu[opcao]:
+            elif "Sair" in opcoes_do_menu[opcao]:
                 input("\nPressione Enter para sair...")
                 if tecla == 'enter':
                     a = False
